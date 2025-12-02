ff5b997997d90e7520cf7da3b5d203a86fcc5374
Harsh Kumar Gupta <harshkrgupta2605@gmail.com>
2025-12-02 23:36:10 +0530

    navbar

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <nav style={{h:"100px",w:"100%",backgroundColor:"black"}}>
+        <h1 style={{color:"white",marginLeft:"20px"}}>HG.</h1>
+        <div>
+          <ul style={{display:"flex",listStyleType:"none",color:"white",gap:"20px",marginLeft:"20px"}}>
+            <li>Home</li>
+            <li>About</li>
+            <li>Services</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+      </nav>
+    </>
   )
 }
 
