04eedc67cfb73d6e584cae2462bf285eb17eaa99
Harsh Kumar Gupta <harshkrgupta2605@gmail.com>
2025-12-08 23:27:03 +0530

    contact

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.css'
 import Navbar from './Component/Navbar'
 import Home from './Component/Home'
+import Contact from './Component/Contact'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Home/>
+      <Contact/>
     </div>
   )
 }
